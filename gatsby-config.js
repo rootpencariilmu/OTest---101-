eea78d6bd5796c510f5bef39c4ff88041280e191
Fahira Fia Anatasya <76982854+rootpencariilmu@users.noreply.github.com>
2021-01-16 07:44:22 +0700

    @rootpencariilmu ~#😷 Play From Home

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
             options: {
                 name: "gatsby-starter-default",
                 short_name: "starter",
-                start_url: "/src/html.js",
+                start_url: "/src/pages/index.js",
                 background_color: "#663399",
                 theme_color: "#663399",
                 display: "minimal-ui",
